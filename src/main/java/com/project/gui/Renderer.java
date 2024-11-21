7839ed88aebf18d7f018d3a1f191a47cc0ab4d1d
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-11-21 11:26:44 +0200

    add scale feature

--- src/main/java/com/project/gui/Renderer.java
+++ src/main/java/com/project/gui/Renderer.java
@@ -9,57 +9,65 @@ import javafx.scene.paint.Color;
 
 public class Renderer {
     final static int BlockWidth = 50;
-    final static int margin = 2;
-    public static void DrawPiece(Piece p , int x , int y){
+    final static int Margin = 2;
+    public static void DrawPiece(Piece p , int x , int y , float scale){
         int XMargin = 0;
         int YMargin = 0;
+        int ScaledWidth = (int)(BlockWidth * scale);
+
         for (int row = 0; row < p.discription.length; row++) {
             for (int col = 0; col < p.discription[row].length; col++) {
                 if(p.discription[row][col]){
                     FXGL.spawn("block",
-                            new SpawnData(x + col * (BlockWidth + YMargin), y + row * (BlockWidth + XMargin))
+                            new SpawnData(x + col * (ScaledWidth + YMargin), y + row * (ScaledWidth + XMargin))
                                     .put("color", p.color)
-                                    .put("width", BlockWidth));
+                                    .put("width", ScaledWidth));
                 }
-                YMargin = margin;
+                YMargin = Margin ;
             }
-            XMargin = margin;
+            XMargin = Margin;
             YMargin = 0;
 
 
         }
     }
-    public static void DrawSquare(Square s , int x , int y){
+    public static void DrawSquare(Square s , int x , int y , float scale ){
         int XMargin = 0;
         int YMargin = 0;
+        int ScaledWidth = (int)(BlockWidth * scale);
+
         for (int row = 0; row < s.data.length; row++) {
             for (int col = 0; col < s.data[row].length; col++) {
-                int BlockX = x + col * (BlockWidth + YMargin);
-                int BlockY = y + row * (BlockWidth + XMargin);
+                int BlockX = x + col * (ScaledWidth + YMargin);
+                int BlockY = y + row * (ScaledWidth + XMargin);
                 if(s.data[row][col] == -1){ // empty block
                     FXGL.spawn("block",
                             new SpawnData(BlockX, BlockY)
                                     .put("color", Color.GRAY)
-                                    .put("width", BlockWidth));
+                                    .put("width", ScaledWidth));
                 }else {
                     // draw piece color
                 }
-                YMargin = margin;
+                YMargin = Margin;
             }
-            XMargin = margin;
+            XMargin = Margin;
             YMargin = 0;
 
 
         }
     }
-    public static RectSize PXSize(Piece p){
-        int h =  p.discription.length    * (BlockWidth + margin);
-        int w =  p.discription[0].length * (BlockWidth + margin);
+    public static RectSize PXSize(Piece p ,float scale){
+        int ScaledWidth = (int)(BlockWidth * scale);
+
+        int h =  p.discription.length    * ScaledWidth + Margin;
+        int w =  p.discription[0].length * ScaledWidth + Margin;
         return new RectSize(w,h);
     }
-    public static RectSize PXSize(Square s){
-        int h =  s.data.length    * (BlockWidth + margin);
-        int w =  s.data[0].length * (BlockWidth + margin);
+    public static RectSize PXSize(Square s , float scale){
+        int ScaledWidth = (int)(BlockWidth * scale);
+
+        int h =  s.data.length    * ScaledWidth + Margin;
+        int w =  s.data[0].length * ScaledWidth + Margin;
         return new RectSize(w,h);
     }
 }
--- src/main/java/com/project/gui/Window.java
+++ src/main/java/com/project/gui/Window.java
@@ -2,11 +2,9 @@ package com.project.gui;
 import com.almasb.fxgl.app.GameApplication ;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.entity.SpawnData;
 import com.project.data.Pieces;
 import com.project.data.RectSize;
 import com.project.data.Square;
-import javafx.scene.paint.Color;
 
 
 public class Window extends GameApplication {
@@ -21,18 +19,20 @@ public class Window extends GameApplication {
     @Override
     protected void initGame() {
         FXGL.getGameWorld().addEntityFactory(new Factory());
-        /*
-        Renderer.DrawPiece(Pieces.NewOPiece() , 10  , 10);
-        Renderer.DrawPiece(Pieces.NewIPiece() , 200 , 10);
-        Renderer.DrawPiece(Pieces.NewZPiece() , 300 , 10);
-        Renderer.DrawPiece(Pieces.NewLPiece() , 500 , 10);
+        float scale = 0.5f;
 
-         */
+        Renderer.DrawPiece(Pieces.NewOPiece() , 10  , 10 , scale);
+        Renderer.DrawPiece(Pieces.NewIPiece() , 200 , 10 , scale);
+        Renderer.DrawPiece(Pieces.NewZPiece() , 300 , 10 , scale);
+        Renderer.DrawPiece(Pieces.NewLPiece() , 500 , 10 , scale);
 
-        Square s = Square.GitInstance();
-        RectSize SquareSize = Renderer.PXSize(s);
+
+
+        Square s = Square.GetInstance();
+
+        RectSize SquareSize = Renderer.PXSize(s , scale);
         Renderer.DrawSquare(s , WindowWidth / 2 - SquareSize.w / 2 ,
-                                WindowHeight/ 2 - SquareSize.h / 2 );
+                                WindowHeight/ 2 - SquareSize.h / 2 , scale);
 
     }
 
