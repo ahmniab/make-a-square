41d579b836187272edd076595c8f8640ef420e83
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-11-25 22:11:38 +0200

    Create PuzzelSolver class

---
+++ src/main/java/com/project/app/PuzzleSolver.java
@@ -0,0 +1,17 @@
+package com.project.app;
+
+import com.project.data.ThreadTracker;
+
+public class PuzzleSolver extends Thread{
+    ThreadTracker tracker;
+    public PuzzleSolver(ThreadTracker tracker){
+        super();
+        this.tracker = tracker;
+
+    }
+    @Override
+    public void run() {
+
+    }
+
+}
