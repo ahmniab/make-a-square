bd313c1f36abc14867b95f7203c47ad2883f646d
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-12-08 19:52:15 +0200

    remove Solver class

--- src/main/java/com/project/app/PuzzleSolver.java
+++ src/main/java/com/project/app/PuzzleSolver.java
@@ -1,8 +1,8 @@
 package com.project.app;
 
-import com.project.data.GlobalData;
-import com.project.data.ThreadState;
-import com.project.data.ThreadTracker;
+import com.project.data.*;
+
+import java.util.Stack;
 
 public class PuzzleSolver extends Thread{
     ThreadTracker tracker;
@@ -15,8 +15,8 @@ public class PuzzleSolver extends Thread{
     public void run() {
         System.out.println(getName()+" Started");
         tracker.threadState = ThreadState.RUNNING;
-        Solver s = new Solver(tracker);
-        boolean success = s.Solve();
+//        Solver s = new Solver(tracker);
+        boolean success = Solve();
         ThreadTracker __main_tracker = GlobalData.getInstance().MainTracker;
         if (success) {
             tracker.threadState = ThreadState.SUCCEEDED;
@@ -29,6 +29,110 @@ public class PuzzleSolver extends Thread{
         }
         __main_tracker.UpdateWindow();
 
+    }
+
+    public boolean IsValid (Piece p, Square s , int x, int y){
+        for(int i=0;i<p.rows;i++)
+            for(int j=0;j<p.columns;j++)
+                if(p.description[i][j])
+                {
+                    int XIndex = x + i;
+                    int YIndex = y + j;
+                    if (XIndex >= 4 )return false;
+                    if (YIndex >= 4 )return false;
+                    if (s.data[XIndex][YIndex] >= 0) return false;
+
+                }
+        return true;
+
+    }
+
+    private Square copySquare(Square s) {
+        Square New = new Square();
+        for (int i=0 ;i<4 ;i++)
+        {
+            System.arraycopy(s.data[i], 0, New.data[i], 0, 4);
+        }
+        return New;
+
+    }
+
+    public Square PutPiece (Piece p,Square s ,int x,int y){
+        Square New = copySquare(s);
+        for (int i = 0; i < p.rows; i++) {
+            for (int j = 0; j < p.columns; j++) {
+                if (p.description[i][j]) {
+                    New.data[x+i][y+j] = p.piece_number ;
+                }
+            }
+        }
+        return New;
+
+    }
+
+    public boolean IsSolved (Square s){
+        for (int i = 0;i<4;i++)
+            for (int j=0;j<4;j++)
+                if(s.data[i][j] == -1)
+                    return false ;
+        return true ;
+    }
+
+    public boolean Solve (){
+        Square s = tracker.square;
+        Piece [] pieces = tracker.pieces;
+        Square S = new Square();
+        Stack<com.project.data.State> stack = new Stack<>();
+        stack.push(new com.project.data.State( 0, 0, 0 ,S));
+        while (!stack.isEmpty()) {
+
+            com.project.data.State cs = stack.pop();
+            if(IsSolved(cs.S)) {
+                return true;
+            }
+
+            if(cs.index >= pieces.length) return false;
+
+            Piece C_piece = pieces[cs.index];
+
+            int PieceRotations = 4-pieces[cs.index].rotations;
+            for (int rotaion = 0;rotaion<PieceRotations; rotaion++) {
+                for (int i = cs.x; i < 4; i++) {
+                    for (int j = (i == cs.x ? cs.y : 0); j < 4; j++) {
+                        if (IsValid(C_piece, cs.S, i, j)) {
+                            Square NewSquare = PutPiece(C_piece, cs.S, i, j);
+                            stack.push(new com.project.data.State(0, 0, cs.index + 1, NewSquare));
+                            s.data = stack.getLast().S.data;
+                            try {
+                                Thread.sleep(300);
+
+                            } catch (Exception e) {
+                                break;
+                            }
+                        }
+
+                    }
+                }
+                if (cs.index == 0) break;
+                PieceOperations.rotateClockWise(pieces[cs.index]);
+
+            }
+
+            pieces[cs.index].ResetRotations();
+
+
+
+            for (int i =0;i<4;i++) {
+                for (int j = 0; j < 4; j++)
+                    System.out.print(stack.peek().S.data[i][j] +"   ");
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+        }
+        return false;
     }
 
 }
